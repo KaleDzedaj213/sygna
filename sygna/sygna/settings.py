47c026d129ae85f5e54df0844a885502d6992ecb
KaleDzedaj213 <natehiggers1@op.pl>
2023-04-26 16:33:04 +0200

    c

--- sygna/sygna/settings.py
+++ sygna/sygna/settings.py
@@ -79,23 +79,16 @@ WSGI_APPLICATION = 'sygna.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'sygna',
-#         'USER': 'sygna',
-#         'PASSWORD': 'Praktyki23#_mysql',
-#         'PORT': 3306,
-#         'OPTIONS': {  
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-#         }  
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sygna',
+        'USER': 'sygna',
+        'PASSWORD': 'Praktyki23#_mysql',
+        'PORT': 3306,
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
     }
 }
 
